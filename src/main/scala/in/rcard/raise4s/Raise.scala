0b6e2f3c0454be8ecbcf91b213428426b65e839b
rcardin <riccardo.cardin@gmail.com>
2024-04-09 21:08:32 +0200

    Introduced the 'raises' infix type

--- src/main/scala/in/rcard/raise4s/Raise.scala
+++ src/main/scala/in/rcard/raise4s/Raise.scala
@@ -2,5 +2,7 @@ package in.rcard.raise4s
 
 trait Raise[-Error]:
   def raise(e: Error): Nothing
+  
+infix type raises[R, Error] = Raise[Error] ?=> R
 
 def raise[Error](e: Error)(using raise: Raise[Error]): Nothing = raise.raise(e)
--- src/test/scala/in/rcard/raise4s/BuildersSpec.scala
+++ src/test/scala/in/rcard/raise4s/BuildersSpec.scala
@@ -6,12 +6,12 @@ import org.scalatest.matchers.should.Matchers
 class BuildersSpec extends AnyFlatSpec with Matchers {
 
   "The either builder" should "create a Left instance" in {
-    val failure: Raise[String] ?=> () => String = () => raise("error")
+    val failure: (() => String) raises String = () => raise("error")
     either(failure) should be(Left("error"))
   }
 
   it should "create a Right instance" in {
-    val success: Raise[String] ?=> () => String = () => "success"
+    val success: (() => String) raises String = () => "success"
     either { success } should be(Right("success"))
   }
 }
