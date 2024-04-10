84505c27ccf6abaf06cec0016cd6027293f67b91
rcardin <riccardo.cardin@gmail.com>
2024-04-10 08:36:12 +0200

    Added test for the 'ensure' and 'ensureNotNull' functions

--- src/test/scala/in/rcard/raise4s/RaiseSpec.scala
+++ src/test/scala/in/rcard/raise4s/RaiseSpec.scala
@@ -5,8 +5,39 @@ import org.scalatest.matchers.should.Matchers
 
 class RaiseSpec extends AnyFlatSpec with Matchers {
 
+  "ensure" should "return Unit if the given condition is met" in {
+    val actual: Int = fold(
+      () => ensure(42 > 0, () => "error"),
+      error => 43,
+      value => 42
+    )
+    actual should be(42)
+  }
+
+  it should "return the error if the condition is not met" in {
+    val actual: Int = fold(
+      () => ensure(42 < 0, () => "error"),
+      error => 43,
+      value => 42
+    )
+    actual should be(43)
+  }
+
   "ensureNotNull" should "return the value if it is not null" in {
-    // TODO: Implement the test
-    true should be(true)
+    val actual: Int = fold(
+      () => ensureNotNull(42, () => "error"),
+      error => 43,
+      value => value
+    )
+    actual should be(42)
+  }
+
+  it should "return the error if the value is null" in {
+    val actual: Int = fold(
+      () => ensureNotNull(null, () => "error"),
+      error => 43,
+      value => 42
+    )
+    actual should be(43)
   }
 }
