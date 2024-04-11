2cf7bae80de29fb206c362380be4c6bad10673b9
rcardin <riccardo.cardin@gmail.com>
2024-04-11 17:20:20 +0200

    WIP added a first version of the OptionRaise type

--- src/main/scala/in/rcard/raise4s/Builders.scala
+++ src/main/scala/in/rcard/raise4s/Builders.scala
@@ -1,15 +1,35 @@
 package in.rcard.raise4s
 
 /** Runs a computation `block` using [[Raise]], and return its outcome as [[Either]].
-  *  - [[Right]] represents success,
-  *  - [[Left]] represents logical failure.
+  *   - [[Right]] represents success,
+  *   - [[Left]] represents logical failure.
   *
   * This function re-throws any exceptions thrown within the [[Raise]] block.
   *
-  * @param block A computation that can raise errors of type `Error`
-  * @tparam A The type of the value returned by the computation
-  * @tparam Error The type of the logical error that can be raised by the computation
-  * @return An [[Either]] representing the outcome of the computation
+  * @param block
+  *   A computation that can raise errors of type `Error`
+  * @tparam A
+  *   The type of the value returned by the computation
+  * @tparam Error
+  *   The type of the logical error that can be raised by the computation
+  * @return
+  *   An [[Either]] representing the outcome of the computation
   */
 def either[A, Error](block: Raise[Error] ?=> A): Either[Error, A] =
   fold(block, error => Left(error), value => Right(value))
+
+class OptionRaise(val raise: Raise[None]) extends Raise[None]
+
+extension [A](option: Option[A])
+  def bind(): A =
+    option.getOrElse(raise(None))
+
+def option[A](block: OptionRaise ?=> A): Option[A] =
+  fold(
+    {
+      given optionRaise: OptionRaise = new OptionRaise(this)
+      block(optionRaise)
+    },
+    identity,
+    Some(_)
+  )
