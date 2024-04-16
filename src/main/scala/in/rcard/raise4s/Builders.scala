2e31462a2f6dc65ab10b449bb8f02af314677a39
rcardin <riccardo.cardin@gmail.com>
2024-04-16 12:58:30 +0200

    Added the builders documentation

--- src/main/scala/in/rcard/raise4s/Builders.scala
+++ src/main/scala/in/rcard/raise4s/Builders.scala
@@ -8,6 +8,22 @@ import scala.util.{Failure, Success, Try}
   *
   * This function re-throws any exceptions thrown within the [[Raise]] block.
   *
+  * <h2>Example</h2>
+  * {{{
+  * val one: Either[Nothing, Int] = Right(1)
+  * val left: Either[String, Int] = Left("error")
+  * val actual = either {
+  *   val x = one.bind()
+  *   val y = recover(
+  *     {
+  *       left.bind()
+  *     },
+  *     { _ => 1 }
+  *   )
+  *   x + y
+  * }
+  * }}}
+  *
   * @param block
   *   A computation that can raise errors of type `Error`
   * @tparam A
@@ -30,6 +46,30 @@ object RaiseOptionPredef:
   extension [A](option: Option[A])(using optionRaise: Raise[None.type])
     def bind(): A = option.getOrElse(raise(None))
 
+/** Runs a computation `block` using [[Raise]], and return its outcome as [[Option]].
+  *   - [[Some]] represents success,
+  *   - [[None]] represents logical failure.
+  * This function re-throws any exceptions thrown within the [[Raise]] block.
+  *
+  * <h2>Example</h2>
+  * {{{
+  * val some: Option[Int] = Some(1)
+  * val none: Option[Int] = None
+  * val actual = option {
+  *   val x = some.bind()
+  *   val y = recover({ none.bind() }, { _ => 1 })
+  *   x + y
+  * }
+  * actual should be(Some(2))
+  * }}}
+  *
+  * @param block
+  *   A computation that can raise errors of type `None.type`
+  * @tparam A
+  *   The type of the value returned by the computation
+  * @return
+  *   An [[Option]] representing the outcome of the computation
+  */
 def option[A](block: Raise[None.type] ?=> A): Option[A] =
   fold(
     {
@@ -46,6 +86,24 @@ object RaiseTryPredef:
       case Success(a) => a
       case Failure(e) => tryRaise.raise(e)
 
+/** Runs a computation `block` using [[Raise]], and return its outcome as [[Try]].
+  *
+  * <h2>Example</h2>
+  * {{{
+  * val one: Try[Int]     = Success(1)
+  * val failure: Try[Int] = Failure(new Exception("error"))
+  * val actual = $try {
+  *   val x = one.bind()
+  *   val y = recover({ failure.bind() }, { _ => 1 })
+  *   x + y
+  * }
+  * actual should be(Success(2))
+  * }}}
+  *
+  * @param block A computation that can raise errors of type `Throwable`
+  * @tparam A The type of the value returned by the computation
+  * @return An [[Try]] representing the outcome of the computation
+  */
 def $try[A](block: Raise[Throwable] ?=> A): Try[A] =
   fold(
     {
