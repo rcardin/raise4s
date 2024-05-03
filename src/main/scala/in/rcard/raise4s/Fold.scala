ca9b97ce09d00c245ba98e3366e6651f3728371a
rcardin <riccardo.cardin@gmail.com>
2024-05-03 15:59:56 +0200

    inlined Raise functions

--- src/main/scala/in/rcard/raise4s/Fold.scala
+++ src/main/scala/in/rcard/raise4s/Fold.scala
@@ -1,38 +1,5 @@
 package in.rcard.raise4s
 
-import scala.util.control.NonFatal
-
-private[raise4s] def _fold[Error, B, A](
-    block: Raise[Error] ?=> A,
-    catchBlock: (throwable: Throwable) => B,
-    recover: (error: Error) => B,
-    transform: (value: A) => B
-) = {
-  given raise: Raise[Error] = new DefaultRaise
-
-  try transform(block)
-  catch
-    case Raised(error) => recover(error.asInstanceOf[Error])
-    case NonFatal(e)   => catchBlock(e)
-    case e: Throwable  => throw e
-}
-
-@deprecated("Use Raise.fold instead", "0.0.3")
-def fold[A, B, Error](
-    block: Raise[Error] ?=> A,
-    catchBlock: (throwable: Throwable) => B,
-    recover: (error: Error) => B,
-    transform: (value: A) => B
-): B =
-  _fold(block, catchBlock, recover, transform)
-
-@deprecated("Use Raise.fold instead", "0.0.3")
-def fold[A, B, Error](
-    block: Raise[Error] ?=> A,
-    recover: (error: Error) => B,
-    transform: (value: A) => B
-): B = _fold(block, ex => throw ex, recover, transform)
-
 private[raise4s] def _mapOrAccumulate[Error, A, B](iterable: Iterable[A])(
     transform: Raise[Error] ?=> A => B
 )(using r: Raise[List[Error]]): List[B] =
