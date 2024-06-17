d701ce3f3e43676336f53d9d655aa06e1edd8d04
rcardin <riccardo.cardin@gmail.com>
2024-06-17 11:12:54 +0200

    Added documentation

--- cats-raise4s/README.md
+++ cats-raise4s/README.md
@@ -13,3 +13,29 @@ libraryDependencies += "in.rcard.raise4s" %% "cats-raise4s" % "0.0.6"
 ```
 
 The library is only available for Scala 3.
+
+## Usage 
+
+In general, the integration lets you use the _Cats_ type classes with the _Raise_ DSL. In detail:
+
+- Use the `Semigroup` type class to combine errors in the `mapOrAccumlate` function.
+
+```scala 3
+case class MyError2(errors: List[String])
+
+given Semigroup[MyError2] with {
+  def combine(error1: MyError2, error2: MyError2): MyError2 =
+    MyError2(error1.errors ++ error2.errors)
+}
+
+val block: List[Int] raises MyError2 =
+  CatsRaise.mapOrAccumulate(List(1, 2, 3, 4, 5)) { value1 =>
+    value1 + 1
+  }
+val actual = Raise.fold(
+  block,
+  error => fail(s"An error occurred: $error"),
+  identity
+)
+actual shouldBe List(2, 3, 4, 5, 6)
+```
--- cats-raise4s/src/main/scala/in/rcard/raise4s/cats/CatsRaise.scala
+++ cats-raise4s/src/main/scala/in/rcard/raise4s/cats/CatsRaise.scala
@@ -4,6 +4,46 @@ import cats.Semigroup
 import in.rcard.raise4s.Raise
 
 object CatsRaise {
+
+  /** Transform every element of `iterable` using the given `transform`, or accumulate all the
+   * occurred errors using the [[Semigroup]] type class defined on the `Error` type.
+   *
+   * <h2>Example</h2>
+   * {{{
+   * case class MyError2(errors: List[String])
+   *
+   * given Semigroup[MyError2] with {
+   *   def combine(error1: MyError2, error2: MyError2): MyError2 =
+   *     MyError2(error1.errors ++ error2.errors)
+   * }
+   *
+   * val block: List[Int] raises MyError2 =
+   *   CatsRaise.mapOrAccumulate(List(1, 2, 3, 4, 5)) { value1 =>
+   *     value1 + 1
+   *   }
+   * val actual = Raise.fold(
+   *   block,
+   *   error => fail(s"An error occurred: $error"),
+   *   identity
+   * )
+   * actual shouldBe List(2, 3, 4, 5, 6)
+   * }}}
+   *
+   * @param iterable
+   *   The collection of elements to transform
+   * @param transform
+   *   The transformation to apply to each element that can raise an error of type `Error`
+   * @param r
+   *   The Raise context
+   * @tparam Error
+   *   The type of the logical error that can be raised. It must have a [[Semigroup]] instance available
+   * @tparam A
+   *   The type of the elements in the `iterable`
+   * @tparam B
+   *   The type of the transformed elements
+   * @return
+   *   A list of transformed elements
+   */
   def mapOrAccumulate[Error: Semigroup, A, B](iterable: Iterable[A])(
       transform: Raise[Error] ?=> A => B
   )(using r: Raise[Error]): List[B] =
