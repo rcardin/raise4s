5e09e8c7e2c1a99dcdd6a63a06b286cf86892009
rcardin <riccardo.cardin@gmail.com>
2024-06-17 09:43:08 +0200

    Added tests for 'mapOrAccumulate' version using the Semigroup[Error] type class

--- cats-raise4s/src/main/scala/in/rcard/raise4s/cats/CatsRaise.scala
+++ cats-raise4s/src/main/scala/in/rcard/raise4s/cats/CatsRaise.scala
@@ -1,10 +1,11 @@
-package in.rcard.raise4s
+package in.rcard.raise4s.cats
 
 import cats.Semigroup
+import in.rcard.raise4s.Raise
 
 object CatsRaise {
   def mapOrAccumulate[Error: Semigroup, A, B](iterable: Iterable[A])(
       transform: Raise[Error] ?=> A => B
   )(using r: Raise[Error]): List[B] =
-    _mapOrAccumulate(iterable, Semigroup[Error].combine)(transform)
+    Raise.mapOrAccumulate(iterable, Semigroup[Error].combine)(transform)
 }
