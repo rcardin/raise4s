7677e6419d46413f7f549cb30a4a6fefac312077
rcardin <riccardo.cardin@gmail.com>
2024-09-04 21:47:20 +0200

    feat(71): First version of the MapError strategy

--- core/src/main/scala/in/rcard/raise4s/Strategies.scala
+++ core/src/main/scala/in/rcard/raise4s/Strategies.scala
@@ -2,13 +2,22 @@ package in.rcard.raise4s
 
 object Strategies {
 
-  /**
-   * An exception that make it easy to throw a logic-typed error if the error is not already a [[Throwable]].
-   *
-   * @param error The logic-typed error to raise
-   * @tparam E The type of the logic-typed error
-   */
+  /** An exception that make it easy to throw a logic-typed error if the error is not already a
+    * [[Throwable]].
+    *
+    * @param error
+    *   The logic-typed error to raise
+    * @tparam E
+    *   The type of the logic-typed error
+    */
   class UnsafeRaiseException[E](val error: E) extends Exception()
-  
+
   type anyRaised = Raise[Any]
+
+  infix type mapTo[EO, EN] = MapError[EO, EN]
+  trait MapError[EO, EN] extends Raise[EO] {
+    def map(error: EO): EN
+
+    def raise(error: EO): Nothing = throw Raised(map(error))
+  }
 }
--- core/src/test/scala/in/rcard/raise4s/StrategiesSpec.scala
+++ core/src/test/scala/in/rcard/raise4s/StrategiesSpec.scala
@@ -1,6 +1,6 @@
 package in.rcard.raise4s
 
-import in.rcard.raise4s.Strategies.anyRaised
+import in.rcard.raise4s.Strategies.{MapError, anyRaised}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -8,10 +8,21 @@ class StrategiesSpec extends AnyFlatSpec with Matchers {
 
   "anyRaised" should "allow defining a strategy that throw any given error" in {
     val lambdaRaisingError: Int raises String = Raise.raise("Oops!")
-    given anyRaised = error => throw new RuntimeException(error.toString)
+    given anyRaised                           = error => throw new RuntimeException(error.toString)
     val ex = intercept[RuntimeException] {
       lambdaRaisingError
     }
     ex.getMessage shouldBe "Oops!"
+  }
+
+  "MapError" should "allow defining a strategy that map an error to another one" in {
+    given MapError[String, Int]               = error => error.length
+    val lambdaRaisingError: Int raises String = Raise.raise("Oops!")
+    val finalLambda: String raises Int = {
+      val r: Int = lambdaRaisingError
+      r.toString
+    }
+    val result: Int | String = Raise.run(finalLambda)
+    result shouldBe 5
   }
 }
